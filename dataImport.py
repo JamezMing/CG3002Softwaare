f11e3e56efa22f07a1484fb81da351e5cccdc15f
JamesMingz <liuxiaoming8839@sina.com>
2017-09-12 11:38:23 +0800

    Added Log File, Fixed some bugs

--- dataImport.py
+++ dataImport.py
@@ -1,17 +1,20 @@
 import numpy as np
 import random
 
-batch_size = 1000
+log_file = open('log_gen', 'a+')
+
 
 class dataFile:
-    def __init__(self, filepath = 'WISDM_ar_v1.1_raw.txt', window_size = 200):
+    def __init__(self, filepath = 'WISDM_ar_v1.1_raw.txt', batch_size = 100, window_size = 100):
         self.dFile = open(filepath)
         self.dict = {'Jogging': 0, 'Walking':1, 'Upstairs':2, 'Downstairs':3, 'Sitting':4, 'Standing':5}
+        self.batch_size = batch_size
+        self.start_index = 0
         self.window_size = window_size
+        self.end_index = self.start_index + self.window_size
+
 
     def init_dataset(self):
-        self.start_index = 0
-        self.end_index = self.start_index + self.window_size
         lines = self.dFile.readlines()
         action_list = []
         previous_action = lines[0].split(',')[1]
@@ -35,49 +38,104 @@ class dataFile:
                 curr_action.append(list(map(float, entry[3:])))
                 previous_action = action_type
                 previous_actor = actor
-
-        self.data_vec = action_list
-        self.label_vec = label_list
+        package = list(zip(action_list, label_list))
+        random.shuffle(package)
+        action_list, label_list = zip(*package)
+        self.curr_data = action_list[0]
+        self.curr_label = label_list[0]
+        self.data_vec = action_list[1:]
+        self.label_vec = label_list[1:]
 
 
     def pop_data(self):
-        data = self.data_vec.pop()
-        label = self.label_vec.pop()
-        package = list(zip(data, label))
-        random.shuffle(package)
-        data, label = zip(*package)
+        data = self.data_vec[0]
+        self.data_vec = self.data_vec[1:]
+        label = [self.label_vec[0]]*len(data)
+        self.label_vec = self.label_vec[1:]
         return data, label
 
+    def hasNextClip(self):
+        if (len(self.data_vec) != 0):
+            return True
+        else:
+            return False
 
-    def pop_serialized_data(self, step = 10):
-
-        data_batch = []
-        label_batch = []
-        curr_batch, curr_label = self.pop_data()
-        print (len(curr_batch))
-        for i in range(0, batch_size):
-            data_batch.append(curr_batch[self.start_index: self.end_index])
-            label_batch.append(label_batch[self.start_index: self.end_index])
-            self.start_index = self.start_index + step
-            self.end_index = self.start_index + self.window_size
-            if self.end_index > len(curr_batch):
-                self.end_index = len(curr_batch)
-                break
-        return data_batch, label_batch
+    def sliding_window_batches(self, step_size = 2):
+        window_data = []
+        window_label = []
+        curr_window = []
+        curr_label = []
 
 
 
+        while (len(window_data) < self.batch_size):
+            log_file.write("End Index :" + str(self.end_index) + " Win Size: " + str(self.window_size) + " Step Size: " + str(
+                step_size) + " Data Length: " + str(len(self.curr_data)) + " Num Clips Left: " + str(len(self.data_vec)) + '\n')
+            #If the clip has not come to the end
+            if(self.end_index + self.window_size + step_size <= len(self.curr_data)):
+                curr_window = self.curr_data[self.start_index:self.end_index]
+                curr_label = [self.curr_label] * len(curr_window)
+                self.start_index = self.start_index + step_size
+                self.end_index = self.start_index + self.window_size
+            #If the Clip has come to the end, put whatever is left into the batch, amend the insufficent ones by copying the last frame, and start the next one in batch from a new clip
+            else:
+                curr_window = self.curr_data[self.start_index:]
+                num_frame_left = self.window_size - len(curr_window)
+                curr_window.append(curr_window[-1] * num_frame_left)
+                if (self.hasNextClip()):
+                    self.curr_data, self.curr_label = self.pop_data()
+                    self.start_index = 0
+                    self.end_index = self.start_index + self.window_size
+
+            window_data.append(curr_window)
+            window_label.append(curr_label)
+
+            for a in window_data:
+                if (len(a) != 100):
+                    log_file.write(str(len(a)) + '\n')
+
+        return window_data, window_label
 
 
 
+        '''pop = False
+        self.start_index = 0
+        self.end_index = self.start_index + window_size
+        while(len(window_data) < self.batch_size):
+            if(pop == True):
+                self.start_index = 0
+                self.end_index = len(data) - len(curr_window)
+                data, label = self.pop_data()
+                pop = False
+            print("End Index :" + str(self.end_index) + " Win Size: " + str(window_size) + " Step Size: " + str(
+                step_size) + " Data Length: " + str(len(data)) + " Num Clips Left: " + str(len(self.data_vec)))
 
+            if self.end_index + window_size + step_size < len(data) and pop == False:
+                curr_window = data[self.start_index:self.end_index]
+                curr_label = [label]*len(curr_window)
+                self.start_index = self.end_index+ step_size
+                self.end_index = self.start_index + window_size
 
+            else:
+                curr_window = data[self.start_index:]
+                curr_label = [label] * len(curr_window)
+                if (self.hasNextClip()):
+                    curr_window.append(data[self.start_index: self.end_index])
+                    curr_label.append([label] * len(curr_window))
+                    pop = True
 
+                else:
+                    curr_window.append(curr_window[-1]*(len(data) - len(curr_window)))
+                    curr_label.append([label]*(len(data) - len(curr_window)))
+            window_data.append(curr_window)
+            window_label.append(curr_label)
+        return window_data, window_label'''
 
-f = dataFile()
-f.init_dataset()
-a, b = f.pop_serialized_data()
-print (a)
+#Sdeing W
+dataSet = dataFile()
+dataSet.init_dataset()
+while(dataSet.hasNextClip()):
+    (a,b) = dataSet.sliding_window_batches()
 
 
 
--- snippetLSTM.py
+++ snippetLSTM.py
@@ -1,7 +1,9 @@
 import tensorflow as tf
 import dataImport
+from tensorflow.contrib import rnn
 
 dataSet = dataImport.dataFile()
+dataSet.init_dataset()
 
 
 
@@ -11,11 +13,22 @@ batch_size = 128
 display_step = 200
 
 # Network Parameters
-num_input = 6 # MNIST data input (img shape: 28*28)
-timesteps = 100 # timesteps
+num_input = 3 # Each time stamp includes a 3-d data
+timesteps = 100 # window size of the data
 num_hidden = 128 # hidden layer num of features
 num_classes = 6 # MNIST total classes (0-9 digits)
 
 # tf Graph input
+#X input dimension: batch_size, window_size, imput_dim
 X = tf.placeholder("float", [None, timesteps, num_input])
 Y = tf.placeholder("float", [None, num_classes])
+weights = {
+    'out': tf.Variable(tf.random_normal([num_hidden, num_classes]))
+}
+biases = {
+    'out': tf.Variable(tf.random_normal([num_classes]))
+}
+lstm_cell = rnn.BasicLSTMCell(num_hidden, forget_bias = 1.0)
+outputs, states = rnn.static_rnn(lstm_cell, X, dtype=tf.float32)
+pred_logits = tf.matmul(outputs[-1], weights['out'] + biases['out'])
+pred = tf.nn.softmax(pred_logits)
